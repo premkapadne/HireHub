f9c357415f074b6259bea6ff4c6a910fc2010194
premkapadne <premkapadne27@gmail.com>
2026-08-31 17:42:51 +0530

    Enable Jpa Auditing

--- src/main/java/com/hirehub/HireHubApplication.java
+++ src/main/java/com/hirehub/HireHubApplication.java
@@ -2,8 +2,10 @@ package com.hirehub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class HireHubApplication {
 
     public static void main(String[] args) {
